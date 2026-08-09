2c09a192de7727edc3321a38af1a7a7e51995ec7
yjs2673 <yjs2673@gmail.com>
2026-08-09 22:23:37 +0900

    Title Image 변경, Player 수치 조정

--- Source/TPSCapture/Components/PlayerStatComponent.h
+++ Source/TPSCapture/Components/PlayerStatComponent.h
@@ -189,10 +189,10 @@ protected:
 	float BaseDefense = 0.f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats")
-	float BaseJumpZVelocity = 400.f;
+	float BaseJumpZVelocity = 600.f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats")
-	float BaseMoveSpeed = 400.f;
+	float BaseMoveSpeed = 600.f;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Stats")
 	float MaxHP = 100.f;
